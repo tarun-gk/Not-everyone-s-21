ad7b12230196a08b86f84208b81f532487ccd9db
tarun-gk <tarun.gadadasu@gmail.com>
2026-06-18 15:41:00 +0530

    day4

---
+++ day4/temp.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class temp {
+      public static void main(String[] var0) {
+      Scanner var = new Scanner(System.in);
+      System.out.println("Enter the current temperature");
+      int temo=var.nextInt();
+      if(temo>=0 && temo<=15){
+        System.out.println("COLD");
+      
+      }
+      else if(temo>15 && temo<=28){
+        System.out.println("Warm");
+      }
+      else if(temo>28){
+        System.out.println("Hot");
+      }
+      else{
+        System.out.println("Freezing");
+      }
+
+}
+}
