3f29107509d2c74080a8930a977e959e85161b40
tarun-gk <tarun.gadadasu@gmail.com>
2026-06-24 13:55:31 +0530

    day1

---
+++ day01/neposzer.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class neposzer {
+    public static void main(String args []){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter number:");
+    int n=sc.nextInt();
+    sc.close();
+    if(n>0){
+        System.out.println("positive");
+
+    }
+    else if(n==0){
+        System.out.println("zero");
+    }
+    else
+    {
+        System.out.println("negative");
+    }
+}
+}
+    
